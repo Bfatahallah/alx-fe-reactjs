91db12c34986c86913f6581fefa56c8f25e168cf
badr fatahallah <b.fatahallah@hotmail.fr>
2025-11-02 00:21:36 +0100

    feat: restructure App component and add Header, MainContent, and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,37 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-
-            <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <WelcomeMessage />
-      <h1>AlX-React-App</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        This is a React application setup for AlX program.
-      </p>
-  <p h1> this is where i will learn react :) 
-    </p><a  href="https://vitejs.dev" target="_blank">
-        <img src={viteLogo} className="logo vite" alt="Vite logo" />
-      </a>
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   )
 }
