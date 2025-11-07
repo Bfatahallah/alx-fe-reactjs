ed399e834945bb9f321862cad90f32e0b15395fa
badr fatahallah <b.fatahallah@hotmail.fr>
2025-11-07 21:26:58 +0100

    refactor: reorganize components and restore UserContext, ProfilePage, UserDetails, and UserInfo

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -2,8 +2,8 @@ import './App.css'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
-import { UserContext } from './UserContext'
-import ProfilePage from './ProfilePage'
+import { UserContext } from './components/UserContext'
+import ProfilePage from './components/ProfilePage'
 
 function App() {
   const userData = { name: 'Jane Doe', email: 'jane.doe@example.com' }
