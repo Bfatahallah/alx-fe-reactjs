fa9fd667c0ad733ad690c6b161ac31727eeab626
badr fatahallah <b.fatahallah@hotmail.fr>
2025-11-07 19:46:46 +0100

    supid checker

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent() {
     return (
         <main style={{ padding: '1rem', fontSize: '1.1rem', backgroundColor: '#e99414ff', borderRadius: '6px', margin: '1rem 0' }}>
-            <p>I would love to visit New York, Paris, and Tokyo.</p>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
 }
