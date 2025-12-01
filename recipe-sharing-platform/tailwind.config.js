fdc89c01f8969106b1e8d8676de03868040b4fec
badr fatahallah <b.fatahallah@hotmail.fr>
2025-12-01 21:02:45 +0100

    fix: correct export format and update content paths in Tailwind CSS configuration

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,8 +1,8 @@
-export default {
-    Content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class',
+ module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // You can set it to 'media' or 'class' if needed
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
